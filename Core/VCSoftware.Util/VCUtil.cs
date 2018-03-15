7269b97c0699e70f7f3b46843116bb95e5343857
vit_chen <j1122321@gmail.com>
2018-03-15 19:10:59 +0800

    日志模块

---
+++ Core/VCSoftware.Util/VCUtil.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VCSoftware.Util.Log;
+
+namespace VCSoftware.Util
+{
+    /// <summary>
+    /// 提供给外部调用
+    /// </summary>
+    public class VCUtil
+    {
+        public static HttpContext UtilContext;
+        public class Logger
+        {
+            public static ILoggerManager loggerMgr;
+
+            /// <summary>
+            /// 跟踪
+            /// </summary>
+            /// <param name="msg"></param>
+            public static void Trace(string msg)
+            {
+                Log(LogLevel.Trace, msg);
+            }
+
+            /// <summary>
+            /// 公共记录
+            /// </summary>
+            /// <param name="level"></param>
+            private static void Log(LogLevel level, string msg)
+            {
+                var logger = loggerMgr.GetLogger();
+                logger.Log(level, msg);
+            }
+        }
+    }
+}
---
+++ VCSoftware.Web/VCStartup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VCSoftware.Util;
+using VCSoftware.Util.Log;
+
+namespace VCSoftware.Web
+{
+    public class VCStartup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();//MVC
+            //日志
+            services.AddScoped<ILoggerManager, Log4NetLoggerManager>();
+            services.AddScoped<ILogger, Log4NetLogger>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public virtual void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            //注册mvc路由
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
