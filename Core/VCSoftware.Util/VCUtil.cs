039259a139bedd4b4e718c98546dffd27975e804
vit_chen <j1122321@gmail.com>
2018-07-06 15:02:12 +0800

    日志模块完成

--- Core/VCSoftware.Util/VCUtil.cs
+++ Core/VCSoftware.Util/VCUtil.cs
@@ -30,6 +30,51 @@ namespace VCSoftware.Util
             }
 
             /// <summary>
+            /// 调试
+            /// </summary>
+            /// <param name="msg"></param>
+            public static void Debug(string msg)
+            {
+                Log(LogLevel.Debug, msg);
+            }
+
+            /// <summary>
+            /// 一般信息
+            /// </summary>
+            /// <param name="msg"></param>
+            public static void Info(string msg)
+            {
+                Log(LogLevel.Info, msg);
+            }
+
+            /// <summary>
+            /// 警告
+            /// </summary>
+            /// <param name="msg"></param>
+            public static void Warn(string msg)
+            {
+                Log(LogLevel.Warn, msg);
+            }
+
+            /// <summary>
+            /// 错误
+            /// </summary>
+            /// <param name="msg"></param>
+            public static void Error(string msg)
+            {
+                Log(LogLevel.Error, msg);
+            }
+
+            /// <summary>
+            /// 严重错误
+            /// </summary>
+            /// <param name="msg"></param>
+            public static void Fatal(string msg)
+            {
+                Log(LogLevel.Fatal, msg);
+            }
+
+            /// <summary>
             /// 公共记录
             /// </summary>
             /// <param name="level"></param>
@@ -48,9 +93,23 @@ namespace VCSoftware.Util
         {
 
             public static IConfiguration Configuration { get; set; }
+
             static Config()
             {
-                Configuration = new ConfigurationBuilder().Add(new JsonConfigurationSource() { Path = "", ReloadOnChange = true }).Build();
+
+            }
+
+            /// <summary>
+            /// 初始化配置，同时拷贝至输出目录
+            /// </summary>
+            /// <param name="path"></param>
+            public static void InitConfig(string basePath, string fileName)
+            {
+                Configuration = new ConfigurationBuilder().SetBasePath(basePath).Add(new JsonConfigurationSource()
+                {
+                    Path = fileName,
+                    ReloadOnChange = true
+                }).Build();
             }
         }
 
--- Test/VCSoftware.Test.Util/Log.cs
+++ Test/VCSoftware.Test.Util/Log.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Hosting;
 using System;
+using System.IO;
+using System.Reflection;
 using VCSoftware.Util;
 using VCSoftware.Util.Log;
 using Xunit;
@@ -7,11 +10,100 @@ namespace VCSoftware.Test.Util
 {
     public class Log
     {
+        /// <summary>
+        /// 跟踪
+        /// </summary>
         [Fact]
         public void Trace()
         {
+            var codebase = Assembly.GetExecutingAssembly().CodeBase;
+            var pathUrlToDllDirectory = Path.GetDirectoryName(codebase);
+            var pathToDllDirectory = new Uri(pathUrlToDllDirectory).LocalPath;
+            var webroot = pathToDllDirectory.ToString().Substring(0, pathToDllDirectory.ToString().IndexOf("bin"));
+
+            VCUtil.Config.InitConfig(webroot,"appsettings.json");
             VCUtil.Logger.loggerMgr = new Log4NetLoggerManager();
             VCUtil.Logger.Trace("woca");
+        }
+
+        /// <summary>
+        /// 调试
+        /// </summary>
+        [Fact]
+        public void Debug()
+        {
+            var codebase = Assembly.GetExecutingAssembly().CodeBase;
+            var pathUrlToDllDirectory = Path.GetDirectoryName(codebase);
+            var pathToDllDirectory = new Uri(pathUrlToDllDirectory).LocalPath;
+            var webroot = pathToDllDirectory.ToString().Substring(0, pathToDllDirectory.ToString().IndexOf("bin"));
+
+            VCUtil.Config.InitConfig(webroot, "appsettings.json");
+            VCUtil.Logger.loggerMgr = new Log4NetLoggerManager();
+            VCUtil.Logger.Debug("woca");
+        }
+
+        /// <summary>
+        /// 一般信息
+        /// </summary>
+        [Fact]
+        public void Info()
+        {
+            var codebase = Assembly.GetExecutingAssembly().CodeBase;
+            var pathUrlToDllDirectory = Path.GetDirectoryName(codebase);
+            var pathToDllDirectory = new Uri(pathUrlToDllDirectory).LocalPath;
+            var webroot = pathToDllDirectory.ToString().Substring(0, pathToDllDirectory.ToString().IndexOf("bin"));
+
+            VCUtil.Config.InitConfig(webroot, "appsettings.json");
+            VCUtil.Logger.loggerMgr = new Log4NetLoggerManager();
+            VCUtil.Logger.Info("woca");
+        }
+
+        /// <summary>
+        /// 警告
+        /// </summary>
+        [Fact]
+        public void Warn()
+        {
+            var codebase = Assembly.GetExecutingAssembly().CodeBase;
+            var pathUrlToDllDirectory = Path.GetDirectoryName(codebase);
+            var pathToDllDirectory = new Uri(pathUrlToDllDirectory).LocalPath;
+            var webroot = pathToDllDirectory.ToString().Substring(0, pathToDllDirectory.ToString().IndexOf("bin"));
+
+            VCUtil.Config.InitConfig(webroot, "appsettings.json");
+            VCUtil.Logger.loggerMgr = new Log4NetLoggerManager();
+            VCUtil.Logger.Warn("woca");
+        }
+
+        /// <summary>
+        /// 错误
+        /// </summary>
+        [Fact]
+        public void Error()
+        {
+            var codebase = Assembly.GetExecutingAssembly().CodeBase;
+            var pathUrlToDllDirectory = Path.GetDirectoryName(codebase);
+            var pathToDllDirectory = new Uri(pathUrlToDllDirectory).LocalPath;
+            var webroot = pathToDllDirectory.ToString().Substring(0, pathToDllDirectory.ToString().IndexOf("bin"));
+
+            VCUtil.Config.InitConfig(webroot, "appsettings.json");
+            VCUtil.Logger.loggerMgr = new Log4NetLoggerManager();
+            VCUtil.Logger.Error("woca");
+        }
+
+        /// <summary>
+        /// 严重错误
+        /// </summary>
+        [Fact]
+        public void Fatal()
+        {
+            var codebase = Assembly.GetExecutingAssembly().CodeBase;
+            var pathUrlToDllDirectory = Path.GetDirectoryName(codebase);
+            var pathToDllDirectory = new Uri(pathUrlToDllDirectory).LocalPath;
+            var webroot = pathToDllDirectory.ToString().Substring(0, pathToDllDirectory.ToString().IndexOf("bin"));
+
+            VCUtil.Config.InitConfig(webroot, "appsettings.json");
+            VCUtil.Logger.loggerMgr = new Log4NetLoggerManager();
+            VCUtil.Logger.Fatal("woca");
         }
     }
 }
